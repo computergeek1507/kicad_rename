ec4f23fbdc1ed8365ef82c7cac00f64b4903fb89
Scott Hanson <scooter_seh@yahoo.com>
2021-11-09 13:12:44 -0500

    kicad project rename app

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace kicad_rename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxProject = new System.Windows.Forms.TextBox();
+            this.textBoxOldName = new System.Windows.Forms.TextBox();
+            this.textBoxNewName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonProject = new System.Windows.Forms.Button();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.openFileDialogProject = new System.Windows.Forms.OpenFileDialog();
+            this.checkBoxCopy = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project:";
+            // 
+            // textBoxProject
+            // 
+            this.textBoxProject.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxProject.Location = new System.Drawing.Point(81, 6);
+            this.textBoxProject.Name = "textBoxProject";
+            this.textBoxProject.Size = new System.Drawing.Size(182, 20);
+            this.textBoxProject.TabIndex = 1;
+            // 
+            // textBoxOldName
+            // 
+            this.textBoxOldName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOldName.Location = new System.Drawing.Point(81, 32);
+            this.textBoxOldName.Name = "textBoxOldName";
+            this.textBoxOldName.Size = new System.Drawing.Size(182, 20);
+            this.textBoxOldName.TabIndex = 2;
+            // 
+            // textBoxNewName
+            // 
+            this.textBoxNewName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxNewName.Location = new System.Drawing.Point(81, 58);
+            this.textBoxNewName.Name = "textBoxNewName";
+            this.textBoxNewName.Size = new System.Drawing.Size(182, 20);
+            this.textBoxNewName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Old Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "New Name:";
+            // 
+            // buttonProject
+            // 
+            this.buttonProject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonProject.Location = new System.Drawing.Point(269, 5);
+            this.buttonProject.Name = "buttonProject";
+            this.buttonProject.Size = new System.Drawing.Size(40, 21);
+            this.buttonProject.TabIndex = 6;
+            this.buttonProject.Text = "...";
+            this.buttonProject.UseVisualStyleBackColor = true;
+            this.buttonProject.Click += new System.EventHandler(this.buttonProject_Click);
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRename.Location = new System.Drawing.Point(173, 84);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(90, 25);
+            this.buttonRename.TabIndex = 7;
+            this.buttonRename.Text = "Rename";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // openFileDialogProject
+            // 
+            this.openFileDialogProject.Filter = "Kicad files (*.pro;*.kicad_pro)|*.pro;*.kicad_pro|All Files|*.*";
+            this.openFileDialogProject.RestoreDirectory = true;
+            this.openFileDialogProject.Title = "Select Project";
+            // 
+            // checkBoxCopy
+            // 
+            this.checkBoxCopy.AutoSize = true;
+            this.checkBoxCopy.Checked = true;
+            this.checkBoxCopy.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxCopy.Location = new System.Drawing.Point(87, 87);
+            this.checkBoxCopy.Name = "checkBoxCopy";
+            this.checkBoxCopy.Size = new System.Drawing.Size(74, 17);
+            this.checkBoxCopy.TabIndex = 8;
+            this.checkBoxCopy.Text = "Copy Files";
+            this.checkBoxCopy.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 116);
+            this.Controls.Add(this.checkBoxCopy);
+            this.Controls.Add(this.buttonRename);
+            this.Controls.Add(this.buttonProject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxNewName);
+            this.Controls.Add(this.textBoxOldName);
+            this.Controls.Add(this.textBoxProject);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(332, 155);
+            this.Name = "Form1";
+            this.Text = "Kicad Rename";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxProject;
+        private System.Windows.Forms.TextBox textBoxOldName;
+        private System.Windows.Forms.TextBox textBoxNewName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonProject;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.OpenFileDialog openFileDialogProject;
+        private System.Windows.Forms.CheckBox checkBoxCopy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kicad_rename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonProject_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogProject.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialogProject.FileName;
+                textBoxProject.Text = filePath;
+                //FileInfo info = new FileInfo(filePath);
+                textBoxNewName.Text = textBoxOldName.Text = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            } 
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            FileInfo project = new FileInfo(textBoxProject.Text);
+            if (!project.Exists)
+            {
+                MessageBox.Show("File Doesn't Exists");
+                return;
+            }
+            if (textBoxNewName.Text == textBoxOldName.Text)
+            {
+                MessageBox.Show("New and Old Names Are the same");
+                return;
+            }
+            foreach (string fileName in Directory.GetFiles(project.Directory.FullName))
+            {
+                // fileName  is the file name
+                string newName = fileName.Replace(textBoxOldName.Text, textBoxNewName.Text);
+                if (fileName == newName)
+                    continue;
+                try
+                {
+                    FileInfo newFile = new FileInfo(newName);
+
+                    if (!newFile.Directory.Exists)
+                    {
+                        newFile.Directory.Create();
+                        //Directory.CreateDirectory(path);
+                    }
+
+
+                    if (checkBoxCopy.Checked)
+                    {
+                        File.Copy(fileName, newName);
+                    }
+                    else
+                    {
+                        File.Move(fileName, newName);
+                    }
+                    Thread.Sleep(100);
+                    ReplaceInFile(newName, textBoxOldName.Text, textBoxNewName.Text);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+
+            foreach (string folderName in Directory.GetDirectories(project.Directory.FullName))
+            {
+                try
+                {
+                    // folderName  is the folder name
+                    string newFolder = folderName.Replace(textBoxOldName.Text, textBoxNewName.Text);
+                    if (folderName == newFolder)
+                        continue;
+                    DirectoryInfo newFile = new DirectoryInfo(newFolder);
+
+                    var diSource = new DirectoryInfo(folderName);
+                    var diTarget = new DirectoryInfo(newFolder);
+
+                    if (checkBoxCopy.Checked)
+                    {
+                        CopyAll(diSource, diTarget);
+                    }
+                    else
+                    {
+                        MoveAll(diSource, diTarget);
+                    }
+  
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+        }
+
+        void ReplaceInFile(string filePath, string searchText, string replaceText)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            string content = reader.ReadToEnd();
+            reader.Close();
+
+            content = Regex.Replace(content, searchText, replaceText);
+
+            StreamWriter writer = new StreamWriter(filePath);
+            writer.Write(content);
+            writer.Close();
+        }
+
+        public void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                //Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        public void MoveAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.MoveTo(Path.Combine(target.FullName, fi.Name));
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                MoveAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+    }
+}
